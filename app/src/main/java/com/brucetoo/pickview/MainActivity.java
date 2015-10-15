3c75448824687673b247ae6cd05be6d9683fcaff
brucetoo <brucetoo14@gmail.com>
2015-10-15 16:43:24 +0800

    Initial commit

---
+++ app/src/main/java/com/brucetoo/pickview/MainActivity.java
@@ -0,0 +1,33 @@
+package com.brucetoo.pickview;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements DatePickerPopWin.OnDatePickCompletedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerPopWin pickerPopWin = new DatePickerPopWin(MainActivity.this,"1907-01-01",MainActivity.this);
+                pickerPopWin.showPopWin(MainActivity.this);
+            }
+        });
+    }
+
+    @Override
+    public void onDatePickCompleted(int year, int month, int day, String dateStr) {
+        Toast.makeText(this,dateStr,Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ app/src/main/java/com/brucetoo/pickview/PickerItem.java
@@ -0,0 +1,14 @@
+package com.brucetoo.pickview;
+
+/**
+ * PickerView item接口类
+ */
+public interface PickerItem {
+
+	/**
+	 * 获取item要显示的文本
+	 * 
+	 * @return
+	 */
+	public String getText();
+}
