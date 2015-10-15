989ebc2e95e604d8db92d64cfc5eaa1c2bfc564a
brucetoo <brucetoo14@gmail.com>
2015-10-15 21:40:11 +0800

    add Province Pick Popwindow

--- app/src/main/java/com/brucetoo/pickview/MainActivity.java
+++ app/src/main/java/com/brucetoo/pickview/MainActivity.java
@@ -2,6 +2,8 @@ package com.brucetoo.pickview;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -9,8 +11,18 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.brucetoo.pickview.datapick.DatePickerPopWin;
+import com.brucetoo.pickview.provincepick.ProvinceModel;
+import com.brucetoo.pickview.provincepick.ProvincePickPopWin;
+import com.brucetoo.pickview.provincepick.utils.ProvinceInfoParserTask;
+import com.brucetoo.pickview.provincepick.utils.ProvinceInfoUtils;
 
-public class MainActivity extends AppCompatActivity implements DatePickerPopWin.OnDatePickCompletedListener {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements DatePickerPopWin.OnDatePickCompletedListener, ProvincePickPopWin.OnAddressPickCompletedListener {
+
+    private ArrayList<ProvinceModel> mProvinceList = null; // 省份列表
+    private String mProvince = null; // 省份
+    private String mCity = null; // 城市
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,14 +53,37 @@ public class MainActivity extends AppCompatActivity implements DatePickerPopWin.
         findViewById(R.id.province).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DatePickerPopWin pickerPopWin = new DatePickerPopWin(MainActivity.this, "1907-01-01", MainActivity.this);
-                pickerPopWin.showPopWin(MainActivity.this);
+                if(null != mProvinceList) {
+                    ProvincePickPopWin pickPopWin = new ProvincePickPopWin(MainActivity.this,
+                            mProvince, mCity, mProvinceList, MainActivity.this);
+                    pickPopWin.showPopWin(MainActivity.this);
+                }
             }
         });
+        ((new ProvinceInfoParserTask(this, mHandler))).execute();// 解析本地地址信息文件
     }
 
     @Override
     public void onDatePickCompleted(int year, int month, int day, String dateStr) {
         Toast.makeText(this,dateStr,Toast.LENGTH_SHORT).show();
+    }
+
+    private Handler mHandler = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message msg) {
+            switch (msg.what) {
+                case ProvinceInfoParserTask.MSG_PARSE_RESULT_CALLBACK: // 解析地址完成
+                    mProvinceList = (ArrayList<ProvinceModel>) msg.obj;
+                    break;
+            }
+            return false;
+        }
+    });
+
+    @Override
+    public void onAddressPickCompleted(String province, String provinceId, String city, String cityId) {
+//        Toast.makeText(this,province+"-"+provinceId+"-"+city+"-"+cityId,Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,ProvinceInfoUtils.matchAddress(this,provinceId,cityId,mProvinceList),Toast.LENGTH_SHORT).show();
+        ProvinceInfoUtils.matchAddress(this,provinceId,cityId,mProvinceList);
     }
 }
